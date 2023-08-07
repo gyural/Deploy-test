78b3cb327082b9f8de72fc530a3a43db79cdf3d4
gyural <dla93190@naver.com>
2023-08-07 19:20:52 +0900

    첫번째 커밋

---
+++ Deploy-test/product-compare/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import ItemsSearchPage from './component/pages/ItemsSearchPage';
+import LoginPage from './component/pages/LoginPage';
+import ItemSelect1 from './component/pages/ItemSelect1';
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route index element={<ItemsSearchPage/>}></Route>
+          <Route path="/signin" element={<LoginPage/>}></Route>
+          <Route path="/select1" element={<ItemSelect1/>}></Route>
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
